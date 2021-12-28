761c2e43ca137eb0dae4c6035147c005351fa109
W2Detroit <sasasa75@bk.ru>
2021-12-28 21:19:39 +0300

    Getting URL  OpenWeather.com and setting JSON from this URL on screen.

--- app/src/main/java/com/example/weatherapplication/MainActivity.java
+++ app/src/main/java/com/example/weatherapplication/MainActivity.java
@@ -2,17 +2,65 @@ package com.example.weatherapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import com.google.android.material.textfield.TextInputEditText;
 
-public class MainActivity extends Activity {
-    private String apiKet = "ac2862a2ad769bc608885cf9a31b072c";
-    private String cityURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    public void onCreate(Bundle savedInstanceState){
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends Activity {
+    private String apiKey = "&appid=ac2862a2ad769bc608885cf9a31b072c";
+    private String cityURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    public String city ;
+    private TextInputEditText cityInput ;
+    private TextInputEditText codeInput;
+    private TextView textView;
+    private JSONObject jsonObject;
+    private Button search;
+    private String jsonWeather;
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cityInput =findViewById(R.id.textInputEditText);
+        codeInput = findViewById(R.id.textInputEditText2);
+        textView = findViewById(R.id.infWeather);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build(); //Async класс
+        StrictMode.setThreadPolicy(policy);
     }
-    
+    public void onClickSearch (View view)  {
+        Thread thread = new Thread(){
+            public void run (){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                OkHTTPActivity okHTTPActivity = new OkHTTPActivity();
+                                String jsonWeather = okHTTPActivity.request("http://api.openweathermap.org/data/2.5/weather?q=Ivanovo&appid=ac2862a2ad769bc608885cf9a31b072c").toString();
+                                textView.setText(jsonWeather);
+                                System.out.print("Json равен" + jsonWeather);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+            }
+        }; thread.start();
+    }
 }
+
+
+
--- app/src/main/java/com/example/weatherapplication/OkHTTPActivity.java
+++ app/src/main/java/com/example/weatherapplication/OkHTTPActivity.java
@@ -1,4 +1,30 @@
 package com.example.weatherapplication;
 
-public class OkHTTPActivity {
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+ public class OkHTTPActivity {
+
+    public Object request (String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url (url)
+                .build();
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+     public static void main(String[] args) throws IOException {
+         OkHTTPActivity okHTTPActivity = new OkHTTPActivity();
+         String str = okHTTPActivity.request("http://api.openweathermap.org/data/2.5/weather?q=Ivanovo&appid=ac2862a2ad769bc608885cf9a31b072c").toString();
+         System.out.println(str);
+     }
 }
