0f44f16a88be325e7d236b881196fb23e5b861cc
W2Detroit <sasasa75@bk.ru>
2022-01-25 18:26:35 +0300

    Displaying the weather for a week and a day without an icon.

--- app/src/main/java/com/example/weatherapplication/JsonWeather.java
+++ app/src/main/java/com/example/weatherapplication/JsonWeather.java
@@ -1,20 +1,20 @@
 package com.example.weatherapplication;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 public class JsonWeather extends JSONObject {
     private JsonArray weather;
+    private String name;
     private JsonObject main;
+    private JsonObject coord;
     JsonWeather (){};
+
+    public JsonObject getCoord() { return coord; }
     public JsonObject getMain() {
         return main;
     }
     public JsonArray getWeather() { return weather; }
+    public String getName () {return name;}
 }
--- app/src/main/java/com/example/weatherapplication/MainActivity.java
+++ app/src/main/java/com/example/weatherapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.StrictMode;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,33 +24,36 @@ import com.google.gson.GsonBuilder;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private final String API_KEY = "&appid=ac2862a2ad769bc608885cf9a31b072c";
-    private final String CITY_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
-    private final String ICON_URL = "http://openweathermap.org/img/wn/";
+    private final String CITY_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
+    private final String ICON_URL = "https://openweathermap.org/img/wn/";
+    private final String URL_CURRENT = "https://api.openweathermap.org/data/2.5/weather?lat=";
     private final String URL_DAILY = "https://api.openweathermap.org/data/2.5/onecall?lat=";
-    private final String CURRENT_WEATHER_lanlon = "https://api.openweathermap.org/data/2.5/weather?lat=";
-    private final String URL_EXCLUDE = "&exclude=current,minutely,hourly,alerts";
+    private final String URL_EXCLUDE = "&exclude=minutely,hourly,alerts";
     private final String FINAL_URL_PNG = "@2x.png";
     private TextInputEditText cityInput;
-    private TextView weatherText, temp_on_celcius, latitude, longitude, her;
-    private ImageView imageWeather;
+    private TextView weatherText, temp_on_celcius, name_city,her;
+    private ImageView imageWeather,icon_weather;
     private LocationManager locationManager;
-
+    private List<DailyWeather> dailyWeathers = new ArrayList();
+    private ListView list_of_days;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageWeather = findViewById(R.id.imageWeather);
-        latitude = findViewById(R.id.latitude);
-        longitude = findViewById(R.id.longithude);
         cityInput = findViewById(R.id.textInputEditText);
         temp_on_celcius = findViewById(R.id.temperature_on_celcius);
         weatherText = findViewById(R.id.weather);
+        name_city = findViewById(R.id.name_city);
+        icon_weather = findViewById(R.id.icon_weather);
         her = findViewById(R.id.her);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE); //обеспечивает доступ к системной службе
         // определения местоположения Android;
-        //  locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 50, this);
+        list_of_days = findViewById(R.id.list_of_days);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build(); //Async класс
         StrictMode.setThreadPolicy(policy);
     }
@@ -66,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         checkEnabled();
     }
 
-    private LocationListener locationListener = new LocationListener() {
+    public LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(@NonNull Location location) {
             try {
@@ -95,36 +99,64 @@ public class MainActivity extends AppCompatActivity {
             checkEnabled();
         }
     };
-        public void getCurrentWeather (String jsonWeather) {
-            GsonBuilder gsonBuilder = new GsonBuilder();
-            gsonBuilder.setPrettyPrinting();
-            Gson gson = gsonBuilder.create();
-            JsonWeather weather = gson.fromJson(jsonWeather, JsonWeather.class);
+
+    public void getCurrentWeather (String jsonWeather) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setPrettyPrinting();
+        Gson gson = gsonBuilder.create();
+            JsonWeather weather =gson.fromJson(jsonWeather, JsonWeather.class);
             temp_on_celcius.setText(( weather.getMain().get("temp").toString() + "°С"));
             weatherText.setText(weather.getWeather().get(0).getAsJsonObject().get("main").getAsString());
+            name_city.setText(weather.getName());
             //Сначала получаем массив с помощью get(0) и представляем его как JsonObject.
             String IdIcon =  weather.getWeather().get(0).getAsJsonObject().get("icon").getAsString();
             Picasso.get()
                     .load(ICON_URL + IdIcon + FINAL_URL_PNG)
                     .into (imageWeather);
         }
-        public void showLocation (Location location) throws IOException {
+        public List <DailyWeather> getLatLonWeather (String jsonWeather)   {
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.setPrettyPrinting();
+            Gson gson = gsonBuilder.create();
+            JsonDailyWeather weather = gson.fromJson(jsonWeather, JsonDailyWeather.class);
+
+            for (Integer i = 0; i <= 7; i++) {
+                dailyWeathers.add (i, new DailyWeather(R.drawable.ic_launcher_background,
+                        weather.getDaily().get(i).getAsJsonObject().get("temp").getAsJsonObject().get("day").getAsString(),
+                        weather.getDaily().get(i).getAsJsonObject().get("temp").getAsJsonObject().get("night").getAsString()
+                        ));
+           // her.setText(weather.getDaily().toString());
+           /* Picasso.get()
+                    .load(ICON_URL + IdIcon + FINAL_URL_PNG)
+                    .into (imageWeather); */
+            }
+            DailyAdapter adapter = new DailyAdapter(this, R.layout.list_item,dailyWeathers);
+            list_of_days.setAdapter(adapter);
+            return dailyWeathers;
+        }
+        public String getLat (Location location) {
+            return String.valueOf(location.getLatitude());
+        }
+        public String getLon (Location location) {
+            return String.valueOf(location.getLongitude());
+        }
+        public void showLocation (Location location) throws IOException{
             if (location == null) {
                 return; }
             else if (location.getProvider().equals(LocationManager.GPS_PROVIDER))  {
-                longitude.setText(String.valueOf(location.getLongitude()));
-                latitude.setText(String.valueOf(location.getLatitude()));
                 OkHTTPActivity okHTTPActivity = new OkHTTPActivity();
-                String jsonWeather = (String) okHTTPActivity.request ((CURRENT_WEATHER_lanlon + String.valueOf(location.getLatitude())
-                        + "&lon="+ String.valueOf(location.getLongitude()) +"&units=metric"+
+                String dailyJsonWeather = (String) okHTTPActivity.request ((URL_DAILY + getLat(location)
+                        + "&lon="+ getLon(location) +"&units=metric"+
                         URL_EXCLUDE + API_KEY));
-                her.setText(jsonWeather);
+              String currentJsonWeather = (String) okHTTPActivity.request(URL_CURRENT +getLat(location) + "&lon=" + getLon(location)+
+                      "&units=metric" + API_KEY);
                 Thread threadGPSWeather = new Thread(){
                     public void run (){
                         runOnUiThread(new Runnable() { // запуск
                             @Override
                             public void run() {
-                                    getCurrentWeather(jsonWeather);
+                                getLatLonWeather(dailyJsonWeather);
+                                getCurrentWeather(currentJsonWeather);
                             }
                         });
                     }
@@ -132,7 +164,7 @@ public class MainActivity extends AppCompatActivity {
             }
             return;
         }
-        private void checkEnabled () {
+    public void checkEnabled () {
             locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
         }
     public String getCurrentURL() {
@@ -141,16 +173,29 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickSearch (View view) throws IOException {
         OkHTTPActivity okHTTPActivity = new OkHTTPActivity();
-        String jsonWeather = okHTTPActivity.request(getCurrentURL()).toString();
+        String currentJsonWeather = (String) okHTTPActivity.request(getCurrentURL());
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setPrettyPrinting();
+        Gson gson = gsonBuilder.create();
+        JsonWeather weather = gson.fromJson(currentJsonWeather, JsonWeather.class);
+        String dailyJsonWeather = (String) okHTTPActivity.request(URL_DAILY + weather.getCoord().get("lat").toString()
+                + "&lon=" + weather.getCoord().get("lon").toString() + "&units=metric" + URL_EXCLUDE + API_KEY);
         Thread threadOneDayWeather = new Thread(){
             public void run (){
                     runOnUiThread(new Runnable() { // запуск
                         @Override
                         public void run() {
 
-                                if (!TextUtils.isEmpty(cityInput.getText().toString())) {
-                                       getCurrentWeather(jsonWeather);
-                                } else {
+                                if (!TextUtils.isEmpty(cityInput.getText().toString()))
+                                 { if (getLatLonWeather(dailyJsonWeather).size() == 7) {
+                                       getCurrentWeather(currentJsonWeather);
+                                       getLatLonWeather(dailyJsonWeather);
+
+                                } else if (getLatLonWeather(dailyJsonWeather).size() > 7){
+                                     getLatLonWeather(dailyJsonWeather).clear();
+                                     getCurrentWeather(currentJsonWeather);
+                                     getLatLonWeather(dailyJsonWeather); }
+                                 } else  {
                                     Toast checkCity = Toast.makeText(getApplicationContext(),"Пожалуйста введите название населенного пункта", Toast.LENGTH_LONG);
                                     checkCity.show();
                                 }
