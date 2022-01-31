f2cdb5aeeec783c3892d9139ccf989bf5b71a1ff
W2Detroit <sasasa75@bk.ru>
2022-01-31 11:05:53 +0300

    Format Change

--- app/src/main/java/com/example/weatherapplication/WeekCollection.java
+++ app/src/main/java/com/example/weatherapplication/WeekCollection.java
@@ -12,7 +12,7 @@ public class WeekCollection {
     public static ArrayList<String> getWeekCollection() {
         ArrayList<String> weekCollection = new ArrayList<>();
         Calendar calendar1 = new GregorianCalendar();
-        SimpleDateFormat simpleDateformat = new SimpleDateFormat("EEEE, dd");
+        SimpleDateFormat simpleDateformat = new SimpleDateFormat("EE, dd");
         weekCollection.add(0,simpleDateformat.format(calendar1.getTime()));
         for (int i = 1; i <= 7; i++) {
           calendar1.add(calendar1.DAY_OF_WEEK, +1);
